95d2a21b7ed8361b95a6c28f3e9138a62484806f
Andrea Limoli <andrealimoli1992@gmail.com>
2019-09-27 12:58:02 +0200

    Improve util_test.

--- util_test.go
+++ util_test.go
@@ -7,49 +7,97 @@ import (
 )
 
 func TestPrintSuccess(t *testing.T) {
-	rescueStdout := os.Stdout
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Error(err)
+
+	// Formatted string => args
+	tests := map[string][]interface{}{
+		"Success":         nil,
+		"Success: %s":     {"flott!"},
+		"Success: %d %s!": {0, "errors"},
 	}
 
-	os.Stdout = w
-
-	printSuccess("Success")
-
-	w.Close()
-	out, err := ioutil.ReadAll(r)
-	if err != nil {
-		t.Error(err)
+	expectedResult := map[string]string{
+		"Success":         "✔ Success\n",
+		"Success: %s":     "✔ Success: flott!\n",
+		"Success: %d %s!": "✔ Success: 0 errors!\n",
 	}
 
-	os.Stdout = rescueStdout
+	for k, v := range tests {
+		rescueStdout := os.Stdout
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Error(err)
+		}
 
-	if rune(out[0]) == successCharacter {
-		t.Error("unexpected character")
+		os.Stdout = w
+
+		printSuccess(k, v...)
+
+		w.Close()
+		out, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Error(err)
+		}
+
+		os.Stdout = rescueStdout
+
+		output := string(out)
+		runes := []rune(output)
+
+		if runes[0] != successCharacter {
+			t.Errorf("unexpected character %c instead of %c", runes[0], successCharacter)
+		}
+
+		if output != expectedResult[k] {
+			t.Errorf("unexpected success message %s instead of %s", output, expectedResult[k])
+		}
 	}
+
 }
 
 func TestPrintFailure(t *testing.T) {
-	rescueStdout := os.Stdout
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Error(err)
+
+	// Formatted string => args
+	tests := map[string][]interface{}{
+		"Failure":         nil,
+		"Failure: %s":     {"uffda!"},
+		"Failure: %d %s!": {5, "errors"},
 	}
 
-	os.Stdout = w
-
-	printFailure("Failure")
-
-	w.Close()
-	out, err := ioutil.ReadAll(r)
-	if err != nil {
-		t.Error(err)
+	expectedResult := map[string]string{
+		"Failure":         "✘ Failure\n",
+		"Failure: %s":     "✘ Failure: uffda!\n",
+		"Failure: %d %s!": "✘ Failure: 5 errors!\n",
 	}
 
-	os.Stdout = rescueStdout
+	for k, v := range tests {
+		rescueStdout := os.Stdout
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Error(err)
+		}
 
-	if rune(out[0]) == failureCharacter {
-		t.Error("unexpected character")
+		os.Stdout = w
+
+		printFailure(k, v...)
+
+		w.Close()
+		out, err := ioutil.ReadAll(r)
+		if err != nil {
+			t.Error(err)
+		}
+
+		os.Stdout = rescueStdout
+
+		output := string(out)
+		runes := []rune(output)
+
+		if runes[0] != failureCharacter {
+			t.Errorf("unexpected character %c instead of %c", runes[0], successCharacter)
+		}
+
+		if output != expectedResult[k] {
+			t.Errorf("unexpected failure message %s instead of %s", output, expectedResult[k])
+		}
+
 	}
 }
