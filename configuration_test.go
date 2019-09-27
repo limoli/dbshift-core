5dfad2698e7f199d4097fdd63edfc6db34195eab
Andrea Limoli <andrealimoli1992@gmail.com>
2019-09-27 14:54:46 +0200

    Improve testing coverage of configuration.

--- configuration_test.go
+++ configuration_test.go
@@ -1,6 +1,10 @@
 package dbshiftcore
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestGetConfiguration(t *testing.T) {
 
@@ -8,6 +12,74 @@ func TestGetConfiguration(t *testing.T) {
 		t.Error("expected missing DBSHIFT_ABS_FOLDER_MIGRATIONS environment variable")
 	}
 
-	// TODO: More tests about configuration and params availability
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	migrationsPath := filepath.Join(wd, "example", "migrations")
+	err = os.Setenv("DBSHIFT_ABS_FOLDER_MIGRATIONS", migrationsPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	cfg, err := getConfiguration()
+	if err != nil {
+		t.Error("expected set DBSHIFT_ABS_FOLDER_MIGRATIONS environment variable")
+	} else if cfg.MigrationsPath != migrationsPath {
+		t.Errorf("expected same migration path: %s != %s", cfg.MigrationsPath, migrationsPath)
+	}
+
+}
+
+func TestGetConfigurationOption(t *testing.T) {
+
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+
+	migrationsPath := filepath.Join(wd, "example", "migrations")
+	err = os.Setenv("DBSHIFT_ABS_FOLDER_MIGRATIONS", migrationsPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Setenv("DBSHIFT_OPTION_IS_CREATE_DISABLED", "true")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Setenv("DBSHIFT_OPTION_IS_DOWNGRADE_DISABLED", "true")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Setenv("DBSHIFT_OPTION_IS_UPGRADE_DISABLED", "true")
+	if err != nil {
+		t.Error(err)
+	}
+
+	cfg, err := getConfiguration()
+	if err != nil {
+		t.Error("expected set DBSHIFT_ABS_FOLDER_MIGRATIONS environment variable")
+	} else if !cfg.Options.IsCreateDisabled {
+		t.Error("expected true value for IsCreateDisabled")
+	} else if !cfg.Options.IsDowngradeDisabled {
+		t.Error("expected true value for IsDowngradeDisabled")
+	} else if !cfg.Options.IsUpgradeDisabled {
+		t.Error("expected true value for IsUpgradeDisabled")
+	}
+}
+
+func TestGetEnvVar(t *testing.T) {
+
+	if result, err := getEnvVar("unavailable_environment_variable!"); err == nil || result != "" {
+		t.Error("expected missing environment variable")
+	}
+
+	if result, err := getEnvVar("PWD"); err != nil || result == "" {
+		t.Error("expected set PWD environment variable")
+	}
 
 }
